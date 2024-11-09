fb5626d9e78be27ce949daa56f5e8dfa289d9379
hp <hp@DESKTOP-QPR8JLM>
2024-11-09 11:18:41 +0530

    Add project files.

---
+++ for_statement_project/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_statement_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i%3 == 0 && (i % 5 != 0))
+                {
+                    Console.WriteLine($"{i} - Fizz");
+                }
+                else if(i%5 == 0 && (i % 3 != 0))
+                {
+                    Console.WriteLine($"{i} - Buzz");
+                }
+                else if( (i % 3 == 0)&& (i % 5 == 0))
+                {
+                    Console.WriteLine($"{i} - FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
